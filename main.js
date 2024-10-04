f577a0b63782299aa1348f79dcdb8bd669056119
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-10-04 17:55:28 +0900

    xhr.open()에 OpenAPI 링크 작성

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 // cc646fdf106947678f84ccf276fbdd99
+
+'use strict';
+let search = document.getElementById('search');
+let news = document.getElementById('news');
+let result = document.getElementById('result');
+
+button.addEventListener('click', function () {
+  let xhr = new XMLHttpRequest();
+  xhr.open(
+    'get',
+    `https://newsapi.org/v2/everything?q=${news.value}&apiKey=cc646fdf106947678f84ccf276fbdd99`,
+    true
+  );
+});
