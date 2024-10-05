cb1a0bb7119cfd7f5667ea327cc179c539bd7cf3
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-10-05 16:49:12 +0900

    입력한 검색어로 뉴스 검색

--- main.js
+++ main.js
@@ -5,11 +5,35 @@ let search = document.getElementById('search');
 let news = document.getElementById('news');
 let result = document.getElementById('result');
 
-button.addEventListener('click', function () {
+search.addEventListener('click', function () {
   let xhr = new XMLHttpRequest();
   xhr.open(
     'get',
     `https://newsapi.org/v2/everything?q=${news.value}&apiKey=cc646fdf106947678f84ccf276fbdd99`,
     true
   );
+  xhr.onload = function () {
+    let getResult = xhr.responseText;
+    let resultObj = JSON.parse(getResult);
+    let article = resultObj['articles'];
+    let resultTxt = '';
+    for (let i = 0; i < article.length; i++) {
+      let title = article[i]['title'];
+      let author = article[i]['author'];
+      let publishedAt = article[i]['publishedAt'];
+      let url = article[i]['url'];
+      let description = article[i]['description'];
+      let urlToImage = article[i]['urlToImage'];
+
+      resultTxt += `
+      <h4>${title}</h4>
+      <span>${author}-${publishedAt}-</span>
+      <a href='${url}'>more</a>
+      <p>${description}</p>
+      <img src='${urlToImage}'>
+      `;
+    }
+    result.innerHTML = resultTxt;
+  };
+  xhr.send();
 });
